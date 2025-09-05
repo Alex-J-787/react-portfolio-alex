de63ec486d4bada3651b835ac6de57a0d915c40a
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-09-05 16:43:16 -0400

    Updated Header, Footer, Nav, updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import Education from "./components/education/Education";
+import Experience from "./components/experience/Experience";
+import Skills from "./components/skills/Skills";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Education />
+      <Experience />
+      <Skills />
+      <Nav />
+      <Footer />
     </>
   );
 };
