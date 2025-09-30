25e81541f426a8bd4d8874f6a5333e72872afdc0
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-09-30 11:11:24 -0400

    Update: Education.jsx and Experience.jsx

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -8,10 +8,15 @@ const Education = () => {
       <p>
         <strong>Hunter College, New York, NY</strong>
         <br></br>
-        <em>M.S. in Geoinformatics (Expected Graduation: December 2025)</em>
+        <em>MS in Geoinformatics (Expected Graduation: December 2025)</em>
         <br></br>Relevant Coursework: Advanced Geoinformatics, Geospatial
         Database Management, Data Analysis & Visualization with R, Web GIS,
         Geocomputation, Quantitative Methods in Geography
+      </p>
+      <p>
+        <strong>Macalester College, St. Paul, MN</strong>
+        <br></br>
+        <em>BA in Political Science (December 2021)</em>
       </p>
     </div>
   );
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -15,7 +15,7 @@ const Experience = () => {
           efficiency across a ten-person web team
         </li>
         <li>
-          Transitioned Hunter College’s website from Plone to WordPress,
+          Transitioned Hunter College's website from Plone to WordPress,
           rebuilding 40+ websites for departments and administrative offices
         </li>
         <li>
@@ -29,7 +29,33 @@ const Experience = () => {
         </li>
         <li>
           Built and improved proficiencies with JavaScript, HTML, CSS, and Regex
-          to improve the efficiency of the team’s web design workflows
+          to improve the efficiency of the team's web design workflows
+        </li>
+      </ul>
+      <h3>
+        Research Intern | Evergreen Exchange, Brooklyn, NY | May to August 2025
+      </h3>
+      <ul>
+        <li>
+          Surveyed dozens of businesses across the Greenpoint-Williamsburg and
+          North Brooklyn IBZs to assess needs
+        </li>
+        <li>
+          Wrote a polished, data-oriented report based on original survey
+          research in North Brooklyn's IBZs, complete with charts, maps, and
+          graphs to communicate data findings about the 40+ survey respondents
+        </li>
+        <li>
+          Assessed issues with Evergreen's database management based on
+          experiences from MS in Geoinformatics program, and made
+          recommendations to staff on ways to improve data quality and potential
+          improvements to a 4000+ entry database
+        </li>
+        <li>
+          Staffed events with members of the North Brooklyn business community,
+          including business owners from hundreds of small and large
+          enterprises, and representatives from utility companies and city
+          agencies
         </li>
       </ul>
     </div>
