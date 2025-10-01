9dfe0003896518f0cdb66f3561d9904c5d4b56c7
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-01 14:11:07 -0400

    Update About.jsx
    added lorem ipsum text as placeholder

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,6 +5,23 @@ const About = () => {
   return (
     <div className="about">
       <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non
+        erat at odio facilisis feugiat. Proin at mi nec turpis suscipit
+        convallis. Integer interdum libero sed ultrices fermentum. Ut ultricies,
+        eros non dignissim gravida, elit ligula posuere nulla, vel gravida est
+        velit a nisl. Suspendisse potenti. Vestibulum faucibus mi sit amet felis
+        pharetra, sed egestas lorem aliquet. Fusce luctus efficitur urna nec
+        aliquam. Etiam commodo, nisl a sodales egestas, urna purus sagittis
+        mauris, non dapibus sem felis vel leo. Sed vitae diam eget justo
+        tincidunt posuere. Phasellus vitae nibh quis urna fermentum ullamcorper
+        in non magna. Nullam at sem eu enim cursus posuere. Ut eu felis eu odio
+        dictum tristique. Cras nec sem nec lectus posuere convallis. Nam
+        fermentum faucibus nibh, vitae dapibus elit ullamcorper in. Donec
+        consequat, sem in laoreet sollicitudin, nulla nibh fermentum elit, nec
+        suscipit turpis lorem nec eros. In eu orci vitae neque tristique feugiat
+        eget at justo.{" "}
+      </p>
     </div>
   );
 };
