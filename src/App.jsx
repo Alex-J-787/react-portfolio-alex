2f5c2e4a473cbb372a57ea866070d9584b83831b
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-02 15:06:35 -0400

    Changed text style to mimic Hunter WP website for now

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,40 @@ const App = () => {
       <Experience />
       <Skills />
       <Contact />
+
+      <div class="btt-background"></div>
+      <div class="icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          aria-labelledby="btt-icon-title"
+          role="img"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <title id="btt-icon-title">Arrow Up</title>
+          <path
+            data-name="layer2"
+            fill="none"
+            stroke="#202020"
+            stroke-miterlimit="10"
+            stroke-width="2"
+            d="M32 10v46"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+          <path
+            data-name="layer1"
+            fill="none"
+            stroke="#202020"
+            stroke-miterlimit="10"
+            stroke-width="2"
+            d="M50 20 L32 4 14 20"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </div>
+      <p class="text">To Top</p>
     </>
   );
 };
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,10 @@ const Contact = () => {
   return (
     <div className="contact">
       <h2>Contact</h2>
+      <p>
+        Email: <br></br>
+        Phone: 347-831-1698
+      </p>
     </div>
   );
 };
