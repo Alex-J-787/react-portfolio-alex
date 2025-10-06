78c0b7e8f9d434a2de69790237483f23af2c6c19
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-06 15:22:45 -0400

    Moved content for resume page to its own page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import React from "react";
-import Topnav from "./components/topnav/Topnav";
-import Header from "./components/resume/header/Header";
-import Education from "./components/resume/education/Education";
-import Projects from "./components/resume/projects/Projects";
-import Experience from "./components/resume/experience/Experience";
-import Skills from "./components/resume/skills/Skills";
-import Footer from "./components/footer/Footer";
-import Divider from "./components/divider/Divider";
+import Resume from "./components/resume/Resume";
 
 const App = () => {
   return (
     <>
-      <div className="topnav">
-        <Topnav />
-      </div>
-      <div className="header-container">
-        <Header />
-      </div>
-      <div className="container">
-        <Divider />
-        <Education />
-        <Divider />
-        <Projects />
-        <Divider />
-        <Experience />
-        <Divider />
-        <Skills />
-      </div>
       <div>
-        <Footer />
+        <Resume />
       </div>
     </>
   );
