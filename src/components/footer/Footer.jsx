5041ab5489ff7f3ca5783fc2c3a9b484d3709466
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-07 15:20:07 -0400

    Added new JSX files for pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,15 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-text">
-        <h3 style={{ marginBottom: "0px" }}>Contact</h3>
-        <p>
-          <a href="mailto:ahd.jacobson@gmail.com" className="footer-text">
-            Email
-          </a>{" "}
-          <br></br>
-        </p>
-      </div>
+      <p>
+        <a href="mailto:ahd.jacobson@gmail.com" className="footer-text">
+          Email
+        </a>{" "}
+        <br></br>
+      </p>
     </div>
   );
 };
