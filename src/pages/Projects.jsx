7aaa3c39f20a3d156298a6c2a7fc78b89196ef66
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-07 16:11:51 -0400

    Update About and Projects to include header and footer

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Topnav from "../components/topnav/Topnav";
+import Footer from "../components/footer/Footer";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="topnav">
+        <Topnav />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
