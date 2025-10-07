9f3df6c5b50f3b0db1c26106c78b112173933529
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-07 14:24:32 -0400

    Updated topnav and title

--- src/components/topnav/Topnav.jsx
+++ src/components/topnav/Topnav.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Topnav = () => {
   return (
     <nav>
-      <a className="topnav-text">Home</a>
-      <a className="topnav-text">About</a>
-      <a href="resume" className="topnav-text">
-        Resume
-      </a>
-      <a className="topnav-text">Projects</a>
+      <div>
+        <a className="topnav-text">Home</a>
+        <a className="topnav-text">About</a>
+        <a href="resume" className="topnav-text">
+          Resume
+        </a>
+        <a className="topnav-text">Projects</a>
+      </div>
     </nav>
   );
 };
