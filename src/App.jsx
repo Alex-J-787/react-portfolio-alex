1dcab90ca460efd5a7bf3228be48c20950766061
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-07 16:09:43 -0400

    Update: Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Resume from "./pages/Resume";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Topnav from "../components/topnav/Topnav";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="topnav">
+        <Topnav />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
