1c7ca867dc9bd5c98c1ad52935baf7759c1b5054
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-22 14:16:30 -0400

    Update NeighborhoodMap.jsx

--- src/components/projects/NeighborhoodMap.jsx
+++ src/components/projects/NeighborhoodMap.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const NeighborhoodMap = () => {
   return (
-    <div className="container">
-      <iframe
-        width="60%"
-        height="800"
-        frameborder="0"
-        src="https://observablehq.com/embed/892870edc930cde8@1348?cell=*"
-      ></iframe>
+    <div>
+      <div className="container">
+        <iframe
+          width="60%"
+          height="800"
+          frameborder="0"
+          src="https://observablehq.com/embed/892870edc930cde8@1348?cell=*"
+        ></iframe>
+      </div>
     </div>
   );
 };
