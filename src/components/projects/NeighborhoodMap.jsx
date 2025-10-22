05bccfd34242850bf0601bc3739ab1b2a06e13aa
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-22 14:06:28 -0400

    Update NeighborhoodMap.jsx

--- src/components/projects/NeighborhoodMap.jsx
+++ src/components/projects/NeighborhoodMap.jsx
@@ -4,7 +4,7 @@ const NeighborhoodMap = () => {
   return (
     <div className="container">
       <iframe
-        width="80%"
+        width="60%"
         height="800"
         frameborder="0"
         src="https://observablehq.com/embed/892870edc930cde8@1348?cell=*"
