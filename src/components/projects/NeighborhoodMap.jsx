bfb4e705106f0e697acc5aa04baa494eaa1ba8e0
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-22 14:01:57 -0400

    Update NeighborhoodMap.jsx

---
+++ src/components/projects/NeighborhoodMap.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NeighborhoodMap = () => {
+  return (
+    <div className="container">
+      <iframe
+        width="80%"
+        height="800"
+        frameborder="0"
+        src="https://observablehq.com/embed/892870edc930cde8@1348?cell=*"
+      ></iframe>
+    </div>
+  );
+};
+
+export default NeighborhoodMap;
