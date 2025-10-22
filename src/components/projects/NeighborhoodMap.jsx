dda6f4f49556851d584592d9ee657d318ee8e754
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-22 14:28:35 -0400

    Update projects folder

--- src/components/projects/NeighborhoodMap.jsx
+++ src/components/projects/NeighborhoodMap.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import "./neighborhoodMap.css";
 
 const NeighborhoodMap = () => {
   return (
-    <div>
-      <div className="container">
+    <div className="map-text-container">
+      <div>
         <iframe
-          width="60%"
+          width="800"
           height="800"
           frameborder="0"
           src="https://observablehq.com/embed/892870edc930cde8@1348?cell=*"
+          className="container"
         ></iframe>
+      </div>
+      <div className="container">
+        <p> Text test text test</p>
       </div>
     </div>
   );
