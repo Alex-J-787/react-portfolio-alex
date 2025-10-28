e986e0907f9e938f729934c881980c57c865d607
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-28 14:46:52 -0400

    Update NeighborhoodMap.jsx

---
+++ src/pages/projectsPages/NeighborhoodMap.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Topnav from "../../components/topnav/Topnav";
+import Footer from "../../components/footer/Footer";
+import NeighborhoodMap from "../../components/projects/neighborhoodMap/NeighborhoodMap";
+import ProjectsPageTitle from "../../components/projects/title/ProjectsPageTitle";
+
+const NeighborhoodMap = () => {
+  return (
+    <div>
+      <div className="topnav">
+        <Topnav />
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="header-container">
+        <ProjectsPageTitle />
+      </div>
+      <div className="container">
+        <NeighborhoodMap />
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default NeighborhoodMap;
