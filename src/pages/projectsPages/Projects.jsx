ae52908b3282e1aa00ed17159330f3f2dd05fea0
Alex-J-787 <158113248+Alex-J-787@users.noreply.github.com>
2025-10-28 14:49:27 -0400

    Update Projects.jsx

--- src/pages/projectsPages/Projects.jsx
+++ src/pages/projectsPages/Projects.jsx
@@ -1,32 +1,7 @@
 import React from "react";
-import Topnav from "../components/topnav/Topnav";
-import Footer from "../components/footer/Footer";
-import NeighborhoodMap from "../components/projects/neighborhoodMap/NeighborhoodMap";
-import ProjectsPageTitle from "../components/projects/title/ProjectsPageTitle";
 
 const Projects = () => {
-  return (
-    <div>
-      <div className="topnav">
-        <Topnav />
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div className="header-container">
-        <ProjectsPageTitle />
-      </div>
-      <div className="container">
-        <NeighborhoodMap />
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Projects;
